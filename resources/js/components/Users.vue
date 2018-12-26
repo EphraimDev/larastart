e7f97df57388da5427dd8f77a1bc3c25b4cd1db8
Aigbefo Ephraim <aigbefoephraim@yahoo.com>
2018-12-26 20:52:33 +0100

    Delete user with ajax request and sweet alert modal

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -37,7 +37,7 @@
                       <i class="fas fa-edit blue"></i>
                     </a>
                     /
-                    <a href="#">
+                    <a href="#" @click="deleteUser(user.id)">
                       <i class="fa fa-trash red"></i>
                     </a>
                   </td>
@@ -159,34 +159,56 @@ export default {
     };
   },
   methods: {
+    deleteUser(id) {
+      Swal({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then(result => {
+        // Send request to the server
+        if (result.value) {
+          this.form
+            .delete("api/user/" + id)
+            .then(() => {
+              Swal("Deleted!", "Your file has been deleted.", "success");
+              Fire.$emit("AfterCreate");
+            })
+            .catch(() => {
+              Swal("Failed", "There was something wrong.", "warning");
+            });
+        }
+      });
+    },
     loadUsers() {
       axios.get("api/user").then(({ data }) => (this.users = data.data));
     },
 
     createUser() {
       this.$Progress.start();
-      this.form.post("api/user")
-      .then(()=> {
-        Fire.$emit('AfterCreate')
-      $("#addNew").modal("hide");
+      this.form
+        .post("api/user")
+        .then(() => {
+          Fire.$emit("AfterCreate");
+          $("#addNew").modal("hide");
 
-      toast({
-        type: "success",
-        title: "User created successfully"
-      });
-      this.$Progress.finish();
-      })
-      .catch(()=>{
-        
-      });
-      
+          toast({
+            type: "success",
+            title: "User created successfully"
+          });
+          this.$Progress.finish();
+        })
+        .catch(() => {});
     }
   },
   created() {
     this.loadUsers();
-    Fire.$on('AfterCreate', ()=>{
-        this.loadUsers()
-    })
+    Fire.$on("AfterCreate", () => {
+      this.loadUsers();
+    });
     //setInterval(() => this.loadUsers(), 3000);
   }
 };
