81d814ee92ac86f018fe833e1c8957aa106b1139
Aigbefo Ephraim <aigbefoephraim@yahoo.com>
2019-01-02 14:21:04 +0100

    Fix authentication bug

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -14,11 +14,4 @@ class EncryptCookies extends Middleware
     protected $except = [
         //
     ];
-
-    /**
-     * Indicates if cookies should be serialized.
-     *
-     * @var bool
-     */
-    protected static $serialize = true;
 }
